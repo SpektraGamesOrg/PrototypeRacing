b664538cd390706cfbde49b5d4f983e8e9184be8
Yusuf Demir <yusuf@spektra.games>
2026-06-20 19:45:06 +0300

    Add gameplay screen and mobile controls

---
+++ Assets/_Game/Scripts/UI/GameplayScreen.cs
@@ -0,0 +1,14 @@
+using System;
+using Core;
+using Save;
+using TMPro;
+using UIManager;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class GameplayScreen : ScreenBase
+    {
+    }
+}
