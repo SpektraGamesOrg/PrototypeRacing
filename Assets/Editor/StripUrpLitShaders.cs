60de3ff48d7bb033f203cdc2eab6e7a85bb0635a
Dogukan Sen <dogukan@spektra.games>
2026-06-21 20:25:50 +0300

    size optimizations

---
+++ Assets/Editor/StripUrpLitShaders.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+class StripUrpLitShaders : IPreprocessShaders
+{
+    public int callbackOrder => 0;
+
+    static readonly HashSet<string> shadersToStrip = new HashSet<string>
+    {
+           "Universal Render Pipeline/Lit",
+    "Universal Render Pipeline/Simple Lit",
+    "Shader Graphs/PhysicalMaterial3DsMax",
+     "Hidden/TerrainEngine/Details/UniversalPipeline/BillboardWavingDoublePass",
+        "Hidden/TerrainEngine/Details/UniversalPipeline/WavingDoublePass",
+        "Hidden/TerrainEngine/Details/UniversalPipeline/Vertexlit",
+        "ProBuilder6/Standart Vertex Color"
+        // Ýhtiyaca göre:
+        // "Universal Render Pipeline/Complex Lit",
+        // "Universal Render Pipeline/Baked Lit",
+    };
+
+    public void OnProcessShader(Shader shader, ShaderSnippetData snippet,
+                                IList<ShaderCompilerData> data)
+    {
+        if (shadersToStrip.Contains(shader.name))
+            data.Clear(); // bu shader'ýn tüm varyantlarýný at
+    }
+}
